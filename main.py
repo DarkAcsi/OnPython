6ec4e283367fb255ababec2516c49f5064eb04d4
DarkAcsi <natakh09@yandex.ru>
2023-06-16 08:07:43 +0300

    Also console

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def update():
     r = random.choice(emojis)
     print(r, r.encode('ascii', 'namereplace').decode()[3:-1])
 
-
-while input() != 'exit':
-    update()
+try:
+    while input() != 'exit':
+        update()
+except EOFError:
+    print("Ошибка во время исполнения")
